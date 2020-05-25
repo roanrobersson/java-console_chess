f1944bd796368919a7bb83042eca70267b49cfcb
Roan Robersson de Oliveira <roanrobersson@gmail.com>
2020-05-25 09:38:02 -0300

    Add ChessException and ChessPosition classes

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,5 @@
 package application;
 
-import boardgame.Board;
-import boardgame.Position;
 import chess.ChessMatch;
 
 public class Program {
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -27,9 +26,13 @@ public class ChessMatch {
 		
 	}
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
 	private void initialSetup() {
-		board.placePiece(new Rook(this.board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King(this.board, Color.BLACK), new Position(0, 4));
-		board.placePiece(new King(this.board, Color.WHITE), new Position(7, 4));
+		placeNewPiece('b', 6, new Rook(this.board, Color.WHITE));
+		placeNewPiece('e', 8, new King(this.board, Color.BLACK));
+		placeNewPiece('e', 1, new King(this.board, Color.WHITE));
 	}
 }
