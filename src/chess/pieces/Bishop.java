d455393a4171c412ec765bebd36e97bb5e08aef7
Roan Robersson de Oliveira <roanrobersson@gmail.com>
2020-07-27 21:39:28 -0300

    Add Bishop class

---
+++ src/chess/pieces/Bishop.java
@@ -0,0 +1,67 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Bishop extends ChessPiece{
+
+	public Bishop(Board board, Color color) {
+		super(board, color);
+	}
+	
+	@Override
+	public String toString() {
+		return "B";
+	}
+	
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] matrix = new boolean[board.getRows()][board.getColumns()];
+		
+		Position p = new Position(0, 0);
+		
+		// left+up
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+		while( board.positionExists(p) && !board.thereIsAPiece(p) ) {
+			matrix[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() - 1, p.getColumn() - 1);
+		}
+		if (board.positionExists(p) && isThereOpponentPiece(p)) {
+			matrix[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// up+right
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		while( board.positionExists(p) && !board.thereIsAPiece(p) ) {
+			matrix[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() - 1, p.getColumn() + 1);
+		}
+		if (board.positionExists(p) && isThereOpponentPiece(p)) {
+			matrix[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// right+down
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+		while( board.positionExists(p) && !board.thereIsAPiece(p) ) {
+			matrix[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() + 1, p.getColumn() + 1);
+		}
+		if (board.positionExists(p) && isThereOpponentPiece(p)) {
+			matrix[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// down+left
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		while( board.positionExists(p) && !board.thereIsAPiece(p) ) {
+			matrix[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() + 1, p.getColumn() - 1);
+		}
+		if (board.positionExists(p) && isThereOpponentPiece(p)) {
+			matrix[p.getRow()][p.getColumn()] = true;
+		}
+		
+		return matrix;
+	}
+}
