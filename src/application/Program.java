3a4f6dc1b346a579203613018b10add7db6dfc66
Roan Robersson de Oliveira <roanrobersson@gmail.com>
2020-09-10 22:54:45 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -40,6 +40,12 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if (chessMatch.getPromoted() != null) {
+					System.out.println("Enter piece for promotion (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			} 
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
